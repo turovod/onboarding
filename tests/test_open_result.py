71373207ef6694545a6df360bc8d4fa05d369dfc
Roman <basharov88roman@gmail.com>
2022-06-23 13:02:25 +0500

    Дополнение readme.md и рефакторинг test_open_result.py

--- tests/test_open_result.py
+++ tests/test_open_result.py
@@ -3,10 +3,16 @@ from source.pages.SearchingResultsPage import SearchingResultsPage
 
 
 def test_click_first_result(driver):
-    MainPage(driver).fill_search_field()
-    SearchingResultsPage(driver).click_first_found_element()
+    main_page = MainPage(driver)
+    main_page.fill_search_field()
+
+    results_page = SearchingResultsPage(driver)
+    results_page.click_first_found_element()
 
 
 def test_click_result(driver):
-    MainPage(driver).fill_search_field()
-    SearchingResultsPage(driver).click_found_element_with_number(3)
+    main_page = MainPage(driver)
+    main_page.fill_search_field()
+
+    results_page = SearchingResultsPage(driver)
+    results_page.click_found_element_with_number(3)
