10ec6bc62790b67207abf4e24cbf67574c6ab283
Loginov <SLoginov@it-one.ru>
2022-07-15 15:58:10 +0300

    Added items from 1 to 6 tasks

--- source/pages/MainPage.py
+++ source/pages/MainPage.py
@@ -13,3 +13,7 @@ class MainPage(BasePage):
     @allure.step("Заполнение поисковой строки текстом")
     def fill_search_field(self):
         BaseElement(self.driver, MainPageLocators.SEARCHING_BOX).fill("Page Object Model" + Keys.ENTER)
+
+    @allure.step("Заполнение поисковой строки произвольным текстом")
+    def fill_search_field_arbitrary_text(self, search_text):
+        BaseElement(self.driver, MainPageLocators.SEARCHING_BOX).fill(search_text + Keys.ENTER)
--- tests/test_open_result.py
+++ tests/test_open_result.py
@@ -10,6 +10,14 @@ def test_click_first_result(driver):
     results_page.click_first_found_element()
 
 
+def test_click_first_result_with_search_text(driver, search_text='turovod.ru'):
+    main_page = MainPage(driver)
+    main_page.fill_search_field_arbitrary_text(search_text)
+
+    results_page = SearchingResultsPage(driver)
+    results_page.click_found_element_with_number(3)
+
+
 def test_click_result(driver):
     main_page = MainPage(driver)
     main_page.fill_search_field()
